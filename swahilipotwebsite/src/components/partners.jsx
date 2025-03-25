22fd984900ac5e01f4baf261df37083870405f5f
sumahya254 <sumahyah732@gmail.com>
2025-03-25 12:20:11 +0300

    Add: added partners

--- swahilipotwebsite/src/components/partners.jsx
+++ swahilipotwebsite/src/components/partners.jsx
@@ -2,6 +2,10 @@ import image4 from '../assets/partner1.jpg';
 import image5 from '../assets/partner2.jpg';
 import image6 from '../assets/partner3.jpg';
 import image7 from '../assets/partner4.jpg';
+import seacom from '../assets/seacom.jpeg';
+import nmk from '../assets/nmk.jpeg';
+import liquid from '../assets/liquid.jpeg';
+import datacamp from '../assets/datacamp.jpeg';
 
 
 export default function Partners() {
@@ -14,6 +18,10 @@ export default function Partners() {
             <img src={image5} alt="Partner 2" className="w-32" />
             <img src={image6} alt="Partner 3" className="w-32" />
             <img src={image7} alt="Partner 3" className="w-32" />
+            <img src={seacom} alt="seacom" className="w-32" />
+            <img src={nmk} alt="nmk" className="w-32" />
+            <img src={liquid} alt="liquid" className="w-32" />
+            <img src={datacamp} alt="datacamp" className="w-32" />
           </div>
         </div>
       </section>
